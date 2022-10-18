df9c898286bc08f7cb695b92fbdba3f18e00e5fe
Franb2004 <111138292+Franb2004@users.noreply.github.com>
2022-10-18 11:13:39 -0300

    Add files via upload

---
+++ Hilo3/Hilo3/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(31, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Iniciar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(428, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(142, 45)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(472, 23)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(20, 19)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(472, 185)
+        Me.TextBox1.TabIndex = 4
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Location = New System.Drawing.Point(142, 94)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(521, 54)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(214, 21)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(142, 200)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(521, 224)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(620, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Hilo3/Hilo3/Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System.Threading
+Imports System.IO
+Public Class Form1
+
+    Dim ruta As String = "D:\Hilo3\"
+
+    Dim archivo As String = "Prueba2.txt"
+    Dim hiloEscrituraArchivo As Thread
+
+    Private synchronizationContext As SynchronizationContext
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        synchronizationContext = SynchronizationContext.Current
+    End Sub
+
+    Private Sub Escribir()
+        Dim fs As FileStream
+        Dim cantidad As UInteger = Integer.Parse(TextBox2.Text)
+        Dim incremento As Double = 100 / cantidad
+        Dim valor As Double = 0
+
+        Try
+            If File.Exists(ruta & archivo) Then
+
+                fs = File.Create(ruta & archivo)
+                fs.Close()
+
+            Else
+
+                Directory.CreateDirectory(ruta)
+                fs = File.Create(ruta & archivo)
+                fs.Close()
+
+            End If
+           Catch ex As Exception
+            MsgBox("Error al crear el archivo")
+        End Try
+        Dim i As Integer
+
+
+        Dim escribir As New StreamWriter(ruta & archivo)
+        Try
+
+            For i = 1 To cantidad
+                escribir.WriteLine("lineas= " & i)
+                 valor += incremento
+                synchronizationContext.Send(New SendOrPostCallback(Sub()
+                                                                       Label1.Text = ProgressBar1.Value & "%"
+                                                                       ProgressBar1.Value = valor
+                                                                       Label1.Refresh()
+                                                                   End Sub), Nothing)
+            Next
+
+        Catch ex As Exception
+            MsgBox("Se cancelo la escritura")
+        Finally
+            escribir.Dispose()
+            synchronizationContext.Send(New SendOrPostCallback(Sub()
+                                                                   Label1.Text = 0 & "%"
+                                                                   ProgressBar1.Value = 0
+                                                                   Label1.Refresh()
+                                                               End Sub), Nothing)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        hiloEscrituraArchivo = New Thread(AddressOf Escribir) With {.IsBackground = True}
+        hiloEscrituraArchivo.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If hiloEscrituraArchivo.IsAlive Then
+            hiloEscrituraArchivo.Abort()
+        End If
+    End Sub
+
+End Class
